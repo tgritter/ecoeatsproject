e08a95fb016a960b3063040aef2e188c571ef36f
Travis Gritter <travis.gritter@gmail.com>
2018-10-30 06:07:05 -0700

    Remove django_heroku import

--- ecoeats/settings.py
+++ ecoeats/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT =path.join(PROJECT_ROOT,'static')
+STATIC_URL =path.join(PROJECT_ROOT,'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
