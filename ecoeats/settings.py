1460ef3385b3fd0fe7f0c64563eeee52e31d3638
Travis Gritter <travis.gritter@gmail.com>
2018-10-30 06:12:23 -0700

    Changed Static Files

--- ecoeats/settings.py
+++ ecoeats/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,8 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT =path.join(PROJECT_ROOT,'static')
-STATIC_URL =path.join(PROJECT_ROOT,'static')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
