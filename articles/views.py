b8186b784b281ff753f24f41405b40550695a990
Travis Gritter <travis.gritter@gmail.com>
2018-10-30 05:51:13 -0700

    First commit

---
+++ articles/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Article
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .import forms
+
+
+# Create your views here.
+def article_list(request):
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article':article})
+
+def edit_post(request, id):
+    article = get_object_or_404(Article, id=id)
+    if request.method == "POST":  
+        form = forms.CreateArticle(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle(instance=article)
+    return render(request, 'articles/article_create.html', {'form':form})
+
+
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form':form})
+
+
---
+++ ecoeats/urls.py
@@ -0,0 +1,32 @@
+"""ecoeats URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from articles import views as article_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^articles/', include('articles.urls')),
+    url(r'^about/$', views.about),
+    url(r'^$', article_views.article_list, name="home")
+]
+
+urlpatterns += staticfiles_urlpatterns()
