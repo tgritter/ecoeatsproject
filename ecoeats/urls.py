d98d918dc05e06591120e5087c765dc39c78bfdd
Travis Gritter <travis.gritter@gmail.com>
2018-10-30 06:17:28 -0700

    Changed Static Files

--- ecoeats/urls.py
+++ ecoeats/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from articles import views as article_views
